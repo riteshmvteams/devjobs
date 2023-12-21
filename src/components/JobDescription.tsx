aee299036123cf5386a23fb61c38cd87f614d9e0
riteshmvteams <ritesh.mvteams@gmail.com>
2023-12-21 14:13:43 +0530

    code refactor

--- src/components/JobDescription.tsx
+++ src/components/JobDescription.tsx
@@ -1,14 +1,13 @@
 import { JobObj } from "@/utils/types";
 import Button from "./Button";
 import JobIntro from "./JobIntro";
+import RequirementListing from "./RequirementListing";
 
 type Props = {
   singleJobPosting: JobObj;
 };
 
 export default function JobDescription({ singleJobPosting }: Props) {
-  console.log(singleJobPosting);
-
   return (
     <div className="max-w-5xl mx-auto w-full bg-white mt-16 mb-12 rounded-lg py-10 px-8 flex flex-col gap-10">
       <div className="flex justify-between items-center">
@@ -23,43 +22,20 @@ export default function JobDescription({ singleJobPosting }: Props) {
         </Button>
       </div>
 
-      <div className="text-gray-500">{singleJobPosting?.description}</div>
+      <p className="text-gray-500">{singleJobPosting?.description}</p>
 
-      <div className="flex flex-col gap-4">
-        <h2 className="text-xl font-semibold">Requirements</h2>
-        <p className="text-gray-500">
-          {singleJobPosting?.requirements?.content}
-        </p>
+      <RequirementListing
+        content={singleJobPosting?.requirements?.content}
+        items={singleJobPosting?.requirements?.items}
+        title="Requirements"
+        type="bullet"
+      />
 
-        <ul className="flex flex-col gap-3 ">
-          {singleJobPosting?.requirements?.items?.map((item, index) => {
-            return (
-              <li key={index} className="flex gap-4 items-center">
-                <span className="block w-1 h-1 rounded-full bg-gray-400 flex-shrink-0"></span>
-                <p className="text-gray-500">{item}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-
-      <div className="flex flex-col gap-4">
-        <h2 className="text-xl font-semibold">What We do</h2>
-        <p className="text-gray-500">{singleJobPosting?.role?.content}</p>
-
-        <ul className="flex flex-col gap-4 items-start">
-          {singleJobPosting?.role?.items?.map((item, index) => {
-            return (
-              <li key={index} className="flex gap-4 items-start">
-                <span className="block text-blue-600 font-semibold flex-shrink-0">
-                  {index + 1}
-                </span>
-                <p className="text-gray-500">{item}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <RequirementListing
+        content={singleJobPosting?.role?.content}
+        items={singleJobPosting?.role?.items}
+        title="What We do"
+      />
     </div>
   );
 }
---
+++ src/components/RequirementListing.tsx
@@ -0,0 +1,37 @@
+type Props = {
+  title: string;
+  content: string;
+  items: string[];
+  type?: string;
+};
+
+export default function RequirementListing({
+  title,
+  content,
+  items,
+  type,
+}: Props) {
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-xl font-semibold">{title}</h2>
+      <p className="text-gray-500">{content}</p>
+
+      <ul className="flex flex-col gap-3 ">
+        {items.map((item, index) => {
+          return (
+            <li key={index} className="flex gap-4 items-center">
+              {type === "bullet" ? (
+                <span className="block w-1 h-1 rounded-full bg-gray-400 flex-shrink-0"></span>
+              ) : (
+                <span className="block text-blue-600 font-semibold flex-shrink-0">
+                  {index + 1}
+                </span>
+              )}
+              <p className="text-gray-500">{item}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
