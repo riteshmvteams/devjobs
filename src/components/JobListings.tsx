26211ee09f45537c490916d22fbd60246e600535
riteshmvteams <ritesh.mvteams@gmail.com>
2023-12-22 12:01:33 +0530

    improved code

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -29,19 +29,28 @@ export default function JobListings() {
 
   return (
     <div className="mt-16 pb-32">
-      <ul className="grid grid-cols-3 gap-x-8 gap-y-20">
-        {listing?.slice(0, show)?.map((job: JobObj) => {
-          return <JobCard key={job.id} job={job} />;
-        })}
-      </ul>
-
-      {show === jobListings.length ? null : (
-        <div className="flex justify-center mt-8">
-          <Button className="bg-blue-500 text-white" onClick={handleLoadMore}>
-            Load More
-          </Button>
+      {listing.length > 0 ? (
+        <ul className="grid grid-cols-3 gap-x-8 gap-y-20">
+          {listing?.slice(0, show)?.map((job: JobObj) => {
+            return <JobCard key={job.id} job={job} />;
+          })}
+        </ul>
+      ) : (
+        <div className="w-full h-[20vh] flex items-center justify-center">
+          <h2 className="text-black dark:text-white font-bold text-xl">
+            No Data Found...
+          </h2>
         </div>
       )}
+
+      {!condition &&
+        (show === jobListings.length ? null : (
+          <div className="flex justify-center mt-8">
+            <Button className="bg-blue-500 text-white" onClick={handleLoadMore}>
+              Load More
+            </Button>
+          </div>
+        ))}
     </div>
   );
 }
